14bc62d9a29e3ae9e3f2d0b2dae6d7150c725d3b
Мария Хватова <mariahvatova@MacBookAir.fritz.box>
2026-03-18 18:36:20 +0100

    Update Akam 2015 model to remove second-stage choice

---
+++ Python/tests/test_ts_akam_par5_trial.py
@@ -0,0 +1,12 @@
+import pytest
+
+from hbayesdm.models import ts_akam_par5_trial
+
+
+def test_ts_akam_par5_trial():
+    _ = ts_akam_par5_trial(
+        data="example", niter=10, nwarmup=5, nchain=1, ncore=1)
+
+
+if __name__ == '__main__':
+    pytest.main()
